73c08af657b3aed3100239ac32f4705062d4906f
sakshi-chakre-04 <sakshichakre0421@gmail.com>
2025-10-24 14:40:53 +0530

    Fourth commit

---
+++ FinalClass1.java
@@ -0,0 +1,15 @@
+final class Base{
+    
+}
+
+// class derived extends Base{      //ERROR
+   
+// }
+
+public class FinalClass1
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
---
+++ FinalClass2.java
@@ -0,0 +1,27 @@
+final class Base
+{
+    public void fun()
+    {
+        System.out.println("Inside Base fun...");
+    }  
+}
+
+class derived 
+{      
+    public Base bobj;           // Composition
+
+    // public Derived()         //concept here 
+    // {
+    //     bobj = new Base();
+    // }
+}
+
+public class FinalClass2
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
+
+// COMPOSITION
