350c51bb9d667207093590bf326feb96452a80be
sakshi-chakre-04 <sakshichakre0421@gmail.com>
2025-10-24 14:48:17 +0530

    fifth commmit

---
+++ InterfaceDemo.java
@@ -0,0 +1,23 @@
+// scenario 2
+interface InterfaceDemo
+{
+    void fun();
+}
+
+interface B
+{
+    void gun();
+}
+
+class Demo implements InterfaceDemo, B      // multiple inheritance
+{
+
+}
+
+class InterfaceDemo
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
